be697f9c1b44468397e2e661f9b0526e9972ebe3
emchandrakant <chandrakantshinde600@gmail.com>
2022-10-26 22:59:07 +0530

    latest stash applied

--- VaccinationCenter/VaccinationCenter/src/main/java/com/microservice/VaccinationCenter/VaccinationCenterApplication.java
+++ VaccinationCenter/VaccinationCenter/src/main/java/com/microservice/VaccinationCenter/VaccinationCenterApplication.java
@@ -2,11 +2,13 @@ package com.microservice.VaccinationCenter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableCircuitBreaker
 public class VaccinationCenterApplication {
 
 	public static void main(String[] args) {
